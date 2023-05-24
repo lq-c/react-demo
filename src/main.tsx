03b21d3feaf5269ebbd143ab0698ab6c31b7c4c4
longqiang <longqiang.chen@cscad.com>
2023-05-24 18:08:05 +0800

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'antd/dist/reset.css';
+import RoutesRoot from './routes/root.tsx';
+import ErrorPage from './404.tsx'
+
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <RoutesRoot></RoutesRoot>,
+    errorElement: <ErrorPage></ErrorPage>
+  },
+  {
+    path:'/404',
+    element: <div>这里什么也没有......你可能走错路了</div>
+  }
+])
 
 import App from './App.tsx'
 import './index.css'
@@ -8,6 +29,7 @@ import './main.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
     <App />
   </React.StrictMode>,
 )
